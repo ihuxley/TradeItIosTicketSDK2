a11e2f3b7f19ab55c7c7f18662421f6b358521e7
Iain Huxley <ihuxley@yahoo-inc.com>
2017-07-20 11:00:17 -0400

    fix SDWI API again

--- TradeItIosTicketSDK2/TradeItBrokerLogoService.swift
+++ TradeItIosTicketSDK2/TradeItBrokerLogoService.swift
@@ -25,8 +25,11 @@ class TradeItBrokerLogoService {
                 return false
         }
 
-        imageView.sd_setImage(with: logoUrl)
-        imageView.sd_setIndicatorStyle(.gray)
+        // diff in this fork, switching to SDWebImage v3.x
+        imageView.setImage(with: logoUrl)
+        imageView.setIndicatorStyle(.gray)
+//        imageView.sd_setImage(with: logoUrl)
+//        imageView.sd_setIndicatorStyle(.gray)
         imageView.sd_setShowActivityIndicatorView(true)
         
         return true
